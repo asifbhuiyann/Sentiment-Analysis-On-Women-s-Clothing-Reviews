ae606b77a374e97d026bb124c654d36848c1bd25
tamim-shadman <tamimshadman2000@gmail.com>
2024-05-18 01:28:48 +0600

    nb_model(api)

--- app.py
+++ app.py
@@ -1,31 +1,31 @@
-import pandas as pd
-from flask import Flask, request, jsonify
 import pickle
+from flask import Flask, request, jsonify
 from sklearn.feature_extraction.text import CountVectorizer
 
 app = Flask(__name__)
 
 # Load the model
-with open('lightGBM_model.pkl', 'rb') as file:
+with open('NaiveBayes_model.pkl', 'rb') as file:
     model = pickle.load(file)
 
-# Initialize CountVectorizer
-count_vectorizer = CountVectorizer()
+# Load the CountVectorizer
+with open('CountVectorizer.pkl', 'rb') as file:
+    vectorizer = pickle.load(file)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         # Get review text from request
-        review_text = request.form['review_text']
+        review_text = request.json['review_text']
 
-        # Fit CountVectorizer and transform the review text
-        review_vector = count_vectorizer.fit_transform([review_text])
+        # Transform the review text using the loaded CountVectorizer
+        review_vector = vectorizer.transform([review_text])
 
         # Make prediction
-        predicted_recommendation = model.predict(review_vector)[0]
+        predicted_recommendation = model.predict(review_vector)
 
-        # Return prediction as JSON
-        return jsonify({'recommended_ind': predicted_recommendation})
+        # Return prediction as JSON (assuming predicted_recommendation is an array)
+        return jsonify({'recommended_ind': int(predicted_recommendation[0])})
     except Exception as e:
         return jsonify({'error': str(e)})
 
